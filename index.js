4852227cb4606cfcea3e75a201c2dc8034c30986
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-18 22:27:31 +0600

    Get data from client side and show data in server side

--- index.js
+++ index.js
@@ -23,12 +23,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const productCollection = client.db('autoHubDB').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            // console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
